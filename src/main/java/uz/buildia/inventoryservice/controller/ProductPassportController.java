6f144d7a8ea006302094c99f2de0914efe43d0c1
NodirUmarov <39554074+NodirUmarov@users.noreply.github.com>
2024-11-25 15:13:41 +0500

    Update ProductPassportController.java

--- src/main/java/uz/buildia/inventoryservice/controller/ProductPassportController.java
+++ src/main/java/uz/buildia/inventoryservice/controller/ProductPassportController.java
@@ -3,6 +3,7 @@ package uz.buildia.inventoryservice.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,4 +25,10 @@ public class ProductPassportController {
       @RequestBody ReportRequest reportRequest) {
     return ResponseEntity.ok(productPassportService.report(reportRequest, scannerId));
   }
+
+  @GetMapping
+  public ResponseEntity<String> test {
+    return ResponseEntity.ok("test");
+  }
+  
 }
